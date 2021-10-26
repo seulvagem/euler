757a6241b6ca168828324ff88b903b3e833c908c
Selvagem <ffelipe.dias@gmail.com>
2021-10-26 10:31:27 -0300

    added base, should make this into it's own repo, and learn how to require from it using lein (even though I should really learn how to use deps.edn...)

---
+++ src/euler/base.clj
@@ -0,0 +1,30 @@
+(ns euler.base)
+
+
+
+(defn get-parse-fn
+  [length]
+  (condp > length 
+   10 #(Integer/parseInt %)
+   19 #(Long/parseLong %)
+   ##Inf bigint))
+
+(defn parse-num
+  [num-str]
+  (let [parse-fn (get-parse-fn (count num-str))]
+    (parse-fn num-str)))
+
+
+
+;; QUEUE
+(defn queue
+  ([]
+   (clojure.lang.PersistentQueue/EMPTY))
+  ([coll]
+   (into (queue) coll)))
+
+;; overwrite default printer for queues
+(defmethod print-method clojure.lang.PersistentQueue [q, w] ; Overload the printer for queues so they look like fish
+  (print-method '<- w)
+  (print-method (seq q) w)
+  (print-method '-< w))
