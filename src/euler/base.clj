04782bf2102e8bdfde26333b46ffd61bdbc547dd
Seulvagem <ffelipe.dias@gmail.com>
2021-10-29 18:43:34 -0300

    conflict fix

--- src/euler/base.clj
+++ src/euler/base.clj
@@ -14,6 +14,31 @@
   (let [parse-fn (get-parse-fn (count num-str))]
     (parse-fn num-str)))
 
+(def x-num->digit
+  "number to digit sequence transducer"
+  (comp (map str)
+        (mapcat seq)))
 
+(defn combinations-of-2
+  [coll]
+  (loop [acc '()
+         sequ coll]
+    (let [x (first sequ)
+          seq2 (next sequ)]
+      (if-not seq2
+        acc
+        (let [combs (map #(list x %) seq2)
+              nacc (into acc combs)]
+          (recur nacc seq2))))))
 
-;; QUEUE
+(defn combinate
+  [f coll]
+  (loop [acc []
+         sequ (seq coll)]
+    (let [x (first sequ)
+          seq2 (next sequ)]
+      (if-not seq2
+        acc
+        (let [combs (map #(cons x %) (f seq2))
+              nacc (into acc combs)]
+          (recur nacc seq2))))))
